97bc262c447e6bdc14908aaf7aefde8075ac59ee
toxicbishop <92860129+toxicbishop@users.noreply.github.com>
2026-06-06 16:34:53 +0530

    ChartLegendContent

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -283,9 +283,10 @@ const ChartLegend = RechartsPrimitive.Legend
 const ChartLegendContent = React.forwardRef<
   HTMLDivElement,
   React.ComponentProps<"div"> &
-    Pick<RechartsPrimitive.LegendProps, "payload" | "verticalAlign"> & {
+    Pick<RechartsPrimitive.LegendProps, "verticalAlign"> & {
       hideIcon?: boolean
       nameKey?: string
+      payload?: any[]
     }
 >(
   (
